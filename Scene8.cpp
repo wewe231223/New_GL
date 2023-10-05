cce1d9141953cfa3dac74380ff28b1fc5b13e218
승범 김 <wewe231223@kpu.ac.kr>
2023-10-06 05:30:31 +0900

    #13 완성( 사각형 피킹 수정 필요? )

--- Scene8.cpp
+++ Scene8.cpp
@@ -23,34 +23,115 @@ RETURNVOID Empty_Rectangle::Initialize()
 	return RETURNVOID();
 }
 
+RETURNVOID Empty_Rectangle::Reset()
+{
+	
+	this->Point1 = { -DEFAULT_SCREEN_WIDTH / 4, DEFAULT_SCREEN_HEIGHT / 4 };
+
+	this->Point2 = { DEFAULT_SCREEN_WIDTH / 4 , DEFAULT_SCREEN_HEIGHT / 4 };
+	this->Point3 = { -DEFAULT_SCREEN_WIDTH / 4,-DEFAULT_SCREEN_HEIGHT / 4 };
+	this->Point4 = { DEFAULT_SCREEN_WIDTH / 4,-DEFAULT_SCREEN_HEIGHT / 4 };
+
+
+
+	this->Color1 = { this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f) };
+	this->Color2 = { this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f) };
+	this->Color3 = { this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f) };
+	this->Color4 = { this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f),this->RG.RandFloat(0.f,1.f) };
+
+
+	return RETURNVOID();
+}
+
 RETURNVOID Empty_Rectangle::Drag(Point2F p)
 {
 
 
+	if (this->Point1_Picked) {
+		Point1.x = p.x;
+		Point1.y = p.y;
 
+		return RETURNVOID();
+	} 
+
+
+	if (this->Point2_Picked) {
+		Point2.x = p.x;
+		Point2.y = p.y;
+
+		return RETURNVOID();
+	}
+
+
+
+
+	if (this->Point3_Picked) {
+		Point3.x = p.x;
+		Point3.y = p.y;
+
+		return RETURNVOID();
+	}
+
+
+
+
+	if (this->Point4_Picked) {
+		Point4.x = p.x;
+		Point4.y = p.y;
+
+		return RETURNVOID();
+	}
+
+
+	if (this->Rect_Picked) {
+		GLfloat dx = p.x - OldDrag.x;
+		GLfloat dy = p.y - OldDrag.y;
+
+		Point1.x += dx;
+		Point1.y += dy;
+
+		Point2.x += dx;
+		Point2.y += dy;
+
+		Point3.x += dx;
+		Point3.y += dy;
+
+		Point4.x += dx;
+		Point4.y += dy;
+
+		this->OldDrag = p;
+
+	}
+
+
+
+
+
+
+
+	return RETURNVOID();
+}
+
+RETURNVOID Empty_Rectangle::Click(Point2F p)
+{
 	if (p.x > this->Point1.x - MouseCorrection && p.x < this->Point1.x + MouseCorrection) {
 		if (p.y > this->Point1.y - MouseCorrection && p.y < this->Point1.y + MouseCorrection) {
 
-				
-			this->Point1.x = p.x;
-			this->Point1.y = p.y;
 
+			this->Point1_Picked = true;
 
+			return RETURNVOID();
 
-			
 		}
-	} 
+	}
 
 	if (p.x > this->Point2.x - MouseCorrection && p.x < this->Point2.x + MouseCorrection) {
 		if (p.y > this->Point2.y - MouseCorrection && p.y < this->Point2.y + MouseCorrection) {
 
 
-
-			this->Point2.x = p.x;
-			this->Point2.y = p.y;
-
-
-
+			this->Point2_Picked = true;
+	
+			return RETURNVOID();
 
 		}
 	}
@@ -59,30 +140,49 @@ RETURNVOID Empty_Rectangle::Drag(Point2F p)
 		if (p.y > this->Point3.y - MouseCorrection && p.y < this->Point3.y + MouseCorrection) {
 
 
-			this->Point3.x = p.x;
-			this->Point3.y = p.y;
-
-
+			this->Point3_Picked = true;
+			return RETURNVOID();
 
 		}
 	}
 
 	if (p.x > this->Point4.x - MouseCorrection && p.x < this->Point4.x + MouseCorrection) {
 		if (p.y > this->Point4.y - MouseCorrection && p.y < this->Point4.y + MouseCorrection) {
+	
+			this->Point4_Picked = true;
 
-			this->Point4.x = p.x;
-			this->Point4.y = p.y;
-
-
+			return RETURNVOID();
 
 
 		}
 	}
 
 
+	if (p.x > this->Point3.x && p.x < this->Point2.x) {
+		if (p.y > this->Point3.y && p.y < this->Point2.y) {
+			this->Rect_Picked = true;
+			this->OldDrag = p;
+		}
+	}
+
+
+
 	return RETURNVOID();
 }
 
+RETURNVOID Empty_Rectangle::ClickRealease()
+{
+	 this->Point1_Picked = false;
+	 this->Point2_Picked = false;
+	 this->Point3_Picked = false;
+	 this->Point4_Picked = false;
+
+
+	 this->Rect_Picked = false;
+
+
+	return RETURNVOID();
+}
 
 
 RETURNVOID Empty_Rectangle::Render()
@@ -119,6 +219,13 @@ RETURNVOID Scene8::Init(){
 	return RETURNVOID();
 }
 
+RETURNVOID Scene8::Reset()
+{
+	this->Rect.Reset();
+
+	return UPDATE;
+}
+
 
 RETURNVOID Scene8::Render()
 {
@@ -136,9 +243,23 @@ RETURNVOID Scene8::DragEvent(Point3F MousePoint)
 	return UPDATE;
 }
 
+RETURNVOID Scene8::ClickEvent(Point3F P)
+{
+	this->Rect.Click(Point2F{ P.x,P.y });
+
+
+	return RETURNVOID();
+}
 
 
 
+RETURNVOID Scene8::ClickRealease()
+{
+
+	this->Rect.ClickRealease();
+	UPDATE;
+	return RETURNVOID();
+}
 
 
 
@@ -165,11 +286,35 @@ RETURNVOID Scene8_CallBackFunctions::Reshape(int w , int h)
 }
 
 
+RETURNVOID Scene8_CallBackFunctions::MouseClick(int button, int state,int x,int y)
+{
+	if (state == GLUT_DOWN) {
 
+		SC8.ClickEvent(WindowCoord_to_GLCoord(x, y));
+		
+		
+	}
+
+	if (state == GLUT_UP) {
+		SC8.ClickRealease();
+	}
+
+	return RETURNVOID();
+}
+
+RETURNVOID Scene8_CallBackFunctions::KeyBoardInput(unsigned char key, int, int)
+{
+	if (key == 'r') {
+		SC8.Reset();
+	}
+
+	return RETURNVOID();
+}
 
 RETURNVOID Scene8_CallBackFunctions::MouseDrag(int x, int y)
 {
 	SC8.DragEvent(WindowCoord_to_GLCoord(x,y));
+	
 	
 	return RETURNVOID();
 }
@@ -185,6 +330,8 @@ CallbackFunc ResisterScene8()
 	result.DrawCall = Scene8_CallBackFunctions::Draw;
 	result.ReShapeCall = Scene8_CallBackFunctions::Reshape;
 	result.MouseDragCall = Scene8_CallBackFunctions::MouseDrag;
+	result.MouseCall = Scene8_CallBackFunctions::MouseClick;
+	result.KeyboardInputCall = Scene8_CallBackFunctions::KeyBoardInput;
 
 	return result;
 }
--- Scene8.h
+++ Scene8.h
@@ -2,7 +2,7 @@
 #include "Object.h"
 #include "RandomGenerater.h"
 
-constexpr auto MouseCorrection = 100.f;
+constexpr auto MouseCorrection = 50.f;
 
 
 
@@ -16,6 +16,17 @@ private:
 	Point2F Point3{};
 	Point2F Point4{};
 
+	GLboolean Point1_Picked = false;
+	GLboolean Point2_Picked = false;
+	GLboolean Point3_Picked = false;
+	GLboolean Point4_Picked = false;
+
+
+
+	Point2F OldDrag{};
+	GLboolean Rect_Picked = false;
+
+
 	Color3f Color1{};
 	Color3f Color2{};
 	Color3f Color3{};
@@ -28,8 +39,11 @@ public:
 
 
 	RETURNVOID Initialize();
+	RETURNVOID Reset();
 
 	RETURNVOID Drag(Point2F);
+	RETURNVOID Click(Point2F);
+	RETURNVOID ClickRealease();
 
 	virtual RETURNVOID Render() override final;
 };
@@ -46,11 +60,14 @@ public:
 
 	RETURNVOID Init();
 
+	RETURNVOID Reset();
 
 
 	RETURNVOID Render();
 
 	RETURNVOID DragEvent(Point3F);
+	RETURNVOID ClickEvent(Point3F);
+	RETURNVOID ClickRealease();
 
 };
 
@@ -60,7 +77,8 @@ namespace Scene8_CallBackFunctions {
 	RETURNVOID Reshape(int, int);
 
 	RETURNVOID MouseDrag(int, int);
-
+	RETURNVOID MouseClick(int,int,int, int);
+	RETURNVOID KeyBoardInput(unsigned char, int, int);
 }
 
 CallbackFunc ResisterScene8();

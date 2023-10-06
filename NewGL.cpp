ad7111f9bb46111e70dbc13c611344790f1029f8
승범 김 <wewe231223@kpu.ac.kr>
2023-10-06 21:53:05 +0900

    3d 시작

--- NewGL.cpp
+++ NewGL.cpp
@@ -19,6 +19,12 @@
 Shader* MainShader;
 
 
+RETURNVOID Draw();
+RETURNVOID ReShape(int w, int h);
+
+GLUquadricObj* qobj;
+
+
 int main(int argc, char** argv) {
 	INIT(&argc, argv);
 	GLW* MAINWINDOW = new GLW(static_cast<int>(DEFAULT_SCREEN_WIDTH),static_cast<int>(DEFAULT_SCREEN_HEIGHT),"THIS IS TEST");
@@ -43,10 +49,18 @@ int main(int argc, char** argv) {
 	//MAINWINDOW->ResisterCallBackFunctions(ResisterScene7());
 
 	// # 12
-	MAINWINDOW->ResisterCallBackFunctions(ResisterScene9());
+	//MAINWINDOW->ResisterCallBackFunctions(ResisterScene9());
 
 	// # 13
 	//MAINWINDOW->ResisterCallBackFunctions(ResisterScene8());
+
+
+
+	glutDisplayFunc(Draw);
+	glutReshapeFunc(ReShape);
+
+
+
 
 	MAINWINDOW->Run();
 
@@ -55,6 +69,27 @@ int main(int argc, char** argv) {
 
 
 
+RETURNVOID Draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	qobj = gluNewQuadric();
+
+	 gluQuadricDrawStyle(qobj, GLU_POINT);
+	 gluQuadricNormals(qobj, GLU_SMOOTH);
+	 gluQuadricOrientation(qobj, GLU_OUTSIDE);
+	 gluSphere(qobj, 1.5, 50, 50); // 객체 만들기
+
+	 glutSwapBuffers();
+
+	return RETURNVOID();
+}
+
+
+RETURNVOID ReShape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	return RETURNVOID();
+}
 
 

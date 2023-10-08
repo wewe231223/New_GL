763872c3041d3fac91b0d0b60514def5336be03e
승범 김 <wewe231223@kpu.ac.kr>
2023-10-09 02:19:56 +0900

    3D shader 2종 작성

--- 3D_Vertex_Shader.glsl
+++ 3D_Vertex_Shader.glsl
@@ -1,1 +1,18 @@
-#version 330 
+#version 330 core
+layout(location = 0) in vec3 in_Position;
+layout(location = 1) in vec3 in_Color;
+
+uniform mat4 transform;
+
+
+
+out vec3 out_Color;
+
+
+
+void main()
+{
+	gl_Position = transform * vec4(in_Position,1.0);
+
+	out_Color = in_Color;
+}

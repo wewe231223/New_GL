a85753709519aba07eb7dda3a468f9bd116b9e12
승범 김 <wewe231223@kpu.ac.kr>
2023-10-09 04:33:04 +0900

    파일 정리 , Scene3D 작성 준비

--- NewGL.cpp
+++ NewGL.cpp
@@ -32,267 +32,13 @@ int rotateKey = 0;
 GLfloat mx = 0.0f;
 GLfloat my = 0.0f;
 
-GLuint triangleVertexArrayObject;
-GLuint shaderProgramID;
-GLuint trianglePositionVertexBufferObjectID, triangleColorVertexBufferObjectID;
-GLuint trianglePositionElementBufferObject;
-
-
 Object3D test{};
 
 
-std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
 
-std::vector< glm::vec3 > vertices;
-std::vector< glm::vec2 > uvs;
-std::vector< glm::vec3 > normals;
 
 bool isCube = true;
 
-char* File_To_Buf(const char* file)
-{
-	ifstream in(file, ios_base::binary);
-
-	if (!in) {
-		cerr << file << "파일 못찾음";
-		exit(1);
-	}
-
-	in.seekg(0, ios_base::end);
-	long len = in.tellg();
-	char* buf = new char[len + 1];
-	in.seekg(0, ios_base::beg);
-
-	int cnt = -1;
-	while (in >> noskipws >> buf[++cnt]) {}
-	buf[len] = 0;
-
-	return buf;
-}
-
-//오브젝트 로드하는 것만 테스트하고 나머지는 알아서 해결해보자
-bool  Load_Object(const char* path) {
-	vertexIndices.clear();
-	uvIndices.clear();
-	normalIndices.clear();
-	vertices.clear();
-	uvs.clear();
-	normals.clear();
-
-	ifstream in(path);
-	if (!in) {
-		cerr << path << "파일 못찾음";
-		exit(1);
-	}
-
-	//vector<char> lineHeader(istream_iterator<char>{in}, {});
-
-	while (in) {
-		string lineHeader;
-		in >> lineHeader;
-		if (lineHeader == "v") {
-			glm::vec3 vertex;
-			in >> vertex.x >> vertex.y >> vertex.z;
-			vertices.push_back(vertex);
-		}
-		else if (lineHeader == "vt") {
-			glm::vec2 uv;
-			in >> uv.x >> uv.y;
-			uvs.push_back(uv);
-		}
-		else if (lineHeader == "vn") {
-			glm::vec3 normal;
-			in >> normal.x >> normal.y >> normal.z;
-			normals.push_back(normal);
-		}
-		else if (lineHeader == "f") {
-			char a;
-			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-
-			for (int i = 0; i < 3; i++)
-			{
-				in >> vertexIndex[i] >> a >> uvIndex[i] >> a >> normalIndex[i];
-				vertexIndices.push_back(vertexIndex[i] - 1);
-				uvIndices.push_back(uvIndex[i] - 1);
-				normalIndices.push_back(normalIndex[i] - 1);
-			}
-		}
-	}
-
-	return true;
-}
-
-bool Make_Shader_Program() {
-	//세이더 코드 파일 불러오기
-	const GLchar* vertexShaderSource = File_To_Buf("3D_Vertex_Shader.glsl");
-	const GLchar* fragmentShaderSource = File_To_Buf("3D_Fragment_Shader.glsl");
-
-	//세이더 객체 만들기
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	//세이더 객체에 세이더 코드 붙이기
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	//세이더 객체 컴파일하기
-	glCompileShader(vertexShader);
-
-	GLint result;
-	GLchar errorLog[512];
-
-	//세이더 상태 가져오기
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
-		cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << endl;
-		return false;
-	}
-
-	//세이더 객체 만들기
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	//세이더 객체에 세이더 코드 붙이기
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	//세이더 객체 컴파일하기
-	glCompileShader(fragmentShader);
-	//세이더 상태 가져오기
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
-		cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << endl;
-		return false;
-	}
-
-	//세이더 프로그램 생성
-	shaderProgramID = glCreateProgram();
-	//세이더 프로그램에 세이더 객체들을 붙이기
-	glAttachShader(shaderProgramID, vertexShader);
-	glAttachShader(shaderProgramID, fragmentShader);
-	//세이더 프로그램 링크
-	glLinkProgram(shaderProgramID);
-
-	//세이더 객체 삭제하기
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	//프로그램 상태 가져오기
-	glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &result);
-	if (!result) {
-		glGetProgramInfoLog(shaderProgramID, 512, NULL, errorLog);
-		cerr << "ERROR: shader program 연결 실패\n" << errorLog << endl;
-		return false;
-	}
-	//세이더 프로그램 활성화
-	glUseProgram(shaderProgramID);
-
-	return true;
-}
-
-bool Set_VAO() {
-	//삼각형을 구성하는 vertex 데이터 - position과 color
-
-	Load_Object("Cube.obj");
-
-	float color[] = {
-	   0.5f, 0.0f, 0.5f,//4
-	   0.0f, 0.0f, 1.0f,//0
-	   0.0f, 0.0f, 0.0f,//3
-
-	   0.5f, 0.0f, 0.5f,//4
-	   0.0f, 0.0f, 0.0f,//3
-	   1.0f, 0.0f, 0.0f,//7
-
-	   0.0f, 1.0f, 0.0f,//2
-	   0.5f, 0.5f, 0.0f,//6
-	   1.0f, 0.0f, 0.0f,//7
-
-	   0.0f, 1.0f, 0.0f,//2
-	   1.0f, 0.0f, 0.0f,//7
-	   0.0f, 0.0f, 0.0f,//3
-
-	   0.0f, 0.5f, 0.5f,//1
-	   1.0f, 1.0f, 1.0f,//5
-	   0.0f, 1.0f, 0.0f,//2
-
-	   1.0f, 1.0f, 1.0f,//5
-	   0.5f, 0.5f, 0.0f,//6
-	   0.0f, 1.0f, 0.0f,//2
-
-	   0.0f, 0.0f, 1.0f,//0
-	   0.5f, 0.0f, 0.5f,//4
-	   0.0f, 0.5f, 0.5f,//1
-
-	   0.5f, 0.0f, 0.5f,//4
-	   1.0f, 1.0f, 1.0f,//5
-	   0.0f, 0.5f, 0.5f,//1
-
-	   0.5f, 0.0f, 0.5f,//4
-	   1.0f, 0.0f, 0.0f,//7
-	   1.0f, 1.0f, 1.0f,//5
-
-	   1.0f, 0.0f, 0.0f,//7
-	   0.5f, 0.5f, 0.0f,//6
-	   1.0f, 1.0f, 1.0f,//5
-
-	   0.0f, 0.0f, 1.0f,//0
-	   0.0f, 0.5f, 0.5f,//1
-	   0.0f, 1.0f, 0.0f,//2
-
-	   0.0f, 0.0f, 1.0f,//0
-	   0.0f, 1.0f, 0.0f,//2
-	   0.0f, 0.0f, 0.0f,//3
-
-
-	};
-
-	//버텍스 배열 오브젝트 (VAO) 이름 생성
-	glGenVertexArrays(1, &triangleVertexArrayObject);
-	//VAO를 바인드한다.
-	glBindVertexArray(triangleVertexArrayObject);
-
-	//Vertex Buffer Object(VBO)를 생성하여 vertex 데이터를 복사한다.
-
-	//버텍스 버퍼 오브젝트 (VBO) 이름 생성
-	glGenBuffers(1, &trianglePositionVertexBufferObjectID);
-	//버퍼 오브젝트를 바인드 한다.
-	glBindBuffer(GL_ARRAY_BUFFER, trianglePositionVertexBufferObjectID);
-	//버퍼 오브젝트의 데이터를 생성
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
-
-	//엘리멘트 버퍼 오브젝트 (EBO) 이름 생성
-	glGenBuffers(1, &trianglePositionElementBufferObject);
-	//버퍼 오브젝트를 바인드 한다.
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, trianglePositionElementBufferObject);
-	//버퍼 오브젝트의 데이터를 생성
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, vertexIndices.size() * sizeof(unsigned int), &vertexIndices[0], GL_STATIC_DRAW);
-
-
-	//버텍스 속성 데이터의 배열을 정의
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	//버텍스 속성 배열을 사용하도록 한다.
-	glEnableVertexAttribArray(0);
-
-
-
-	////칼라 버퍼 오브젝트 (VBO) 이름 생성
-	//glGenBuffers(1, &triangleColorVertexBufferObjectID);
-	////버퍼 오브젝트를 바인드 한다.
-	//glBindBuffer(GL_ARRAY_BUFFER, triangleColorVertexBufferObjectID);
-	////버퍼 오브젝트의 데이터를 생성
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
-
-
-	////버퍼 오브젝트를 바인드 한다.
-	//glBindBuffer(GL_ARRAY_BUFFER, triangleColorVertexBufferObjectID);
-	////버텍스 속성 데이터의 배열을 정의
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	////버텍스 속성 배열을 사용하도록 한다.
-	//glEnableVertexAttribArray(1);
-
-
-	glBindVertexArray(0);
-
-
-	return true;
-}
 
 //--- 콜백 함수: 그리기 콜백 함수 
 GLvoid drawScene()
@@ -302,28 +48,10 @@ GLvoid drawScene()
 	// 설정된 색으로 전체를 칠하기
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//isCulling ? glEnable(GL_DEPTH_TEST) : glDisable(GL_DEPTH_TEST);
-	//isCulling ? glEnable(GL_CULL_FACE) : glDisable(GL_CULL_FACE);
-	//isFill ? glPolygonMode(GL_FRONT_AND_BACK, GL_FILL) : glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-
-	//glm::mat4 TR = glm::mat4(1.0f);
-	//TR = glm::translate(TR, glm::vec3(xMove, yMove, zMove));
-	//TR = glm::rotate(TR, glm::radians(30.0f + xRotateAni), glm::vec3(1.0, 0.0, 0.0));
-	//TR = glm::rotate(TR, glm::radians(-30.0f + yRotateAni), glm::vec3(0.0, 1.0, 0.0));
-	//unsigned int modelLocation = glGetUniformLocation(MainShader->GetInfo(ShaderProgramID), "transform"); //--- 버텍스 세이더에서모델 변환 위치 가져오기
-	//glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(TR)); //--- modelTransform 변수에 변환 값 적용하기
-
-	//// 사용할 VAO 불러오기
-	//glBindVertexArray(triangleVertexArrayObject);
-
-	////배열 데이터로부터 프리미티브 렌더링 하기
-	//glDrawElements(GL_TRIANGLES, vertexIndices.size(), GL_UNSIGNED_INT, 0);
 
 	test.Render();
 
-	//glDrawArrays(GL_TRIANGLES, 0, 12);
-	// 화면에 출력하기
+
 	glutSwapBuffers();
 }
 
@@ -337,13 +65,13 @@ GLvoid TimerFunction1(int value)
 {
 	glutPostRedisplay(); // 화면 재 출력
 	if (rotateKey == 1)
-		xRotateAni += 0.5f;
+		xRotateAni += 1.f;
 	if (rotateKey == 2)
-		xRotateAni -= 0.5f;
+		xRotateAni -= 1.f;
 	if (rotateKey == 3)
-		yRotateAni += 0.5f;
+		yRotateAni += 1.f;
 	if (rotateKey == 4)
-		yRotateAni -= 0.5f;
+		yRotateAni -= 1.f;
 	glutTimerFunc(100, TimerFunction1, 1);
 }
 
@@ -352,26 +80,23 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 	switch (key) {
 	case 'c':
 		isCube = true;
-		Set_VAO();
 		break;
 	case 'p':
 		isCube = false;
-		Set_VAO();
 		break;
 	case 'h':
 		isCulling = 1 - isCulling;
 		break;
 	case 'x':
-		rotateKey = 1;
-		glutTimerFunc(100, TimerFunction1, 1);
+		test.Transition(RotateX, glm::vec3(1.f, 0.f, 0.f));
 		break;
 	case 'X':
 		rotateKey = 2;
 		glutTimerFunc(100, TimerFunction1, 1);
 		break;
 	case 'y':
-		rotateKey = 3;
-		glutTimerFunc(100, TimerFunction1, 1);
+		
+	
 		break;
 	case 'Y':
 		rotateKey = 4;
@@ -405,6 +130,10 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 	glutPostRedisplay(); //--- 배경색이 바뀔때마다 출력 콜백함수를 호출하여 화면을 refresh 한다
 }
 
+void Idle() {
+	test.Transition(RotateY, glm::vec3(0.f, 1.f, 0.f));
+	UPDATE;
+}
 
 void Mouse(int button, int state, int x, int y)
 {
@@ -413,7 +142,6 @@ void Mouse(int button, int state, int x, int y)
 		mx = (x - half_w) / half_w;
 		my = (half_w - y) / half_w;
 	}
-	Set_VAO();
 	glutPostRedisplay();
 }
 
@@ -448,22 +176,19 @@ int main(int argc, char** argv)
 
 	MainShader->LINK_SHADER();
 
+	
 
-
-	test.Initialize("Cube.obj", MainShader->GetInfo(ShaderProgramID));
+	test.Initialize("teapot.obj", MainShader->GetInfo(ShaderProgramID));
+	test.Transition(ObjectScale, glm::vec3(0.3f, 0.3f, 0.3f));
 	test.Buffering();
 
 
-
-	//if (!Set_VAO()) {
-	//	cerr << "Error: VAO 생성 실패" << endl;
-	//	std::exit(EXIT_FAILURE);
-	//}
 
 	glutDisplayFunc(drawScene);													// 출력 함수의 지정
 	glutReshapeFunc(Reshape);														// 다시 그리기 함수 지정
 	glutKeyboardFunc(Keyboard);
 	glutMouseFunc(Mouse);
+	glutIdleFunc(Idle);
 	glutMainLoop();																			// 이벤트 처리 시작
 }
 
--- Object3D.cpp
+++ Object3D.cpp
@@ -1,1 +1,238 @@
 #include "Object3D.h"
+
+
+RETURNVOID Object3D::Initialize(const char* path, GLuint ShaderProgramId) {
+	this->ShaderProgramId = ShaderProgramId;
+
+	this->File.open(path);
+
+	if (!this->File) {
+		std::cerr << "OBJ FILE ERROR!" << std::endl;
+		exit(-1);
+	}
+
+
+	return this->Resister();
+}
+
+
+
+RETURNVOID Object3D::Resister() {
+	std::vector<glm::vec3>().swap(this->Verties);
+	std::vector<glm::vec2>().swap(this->UVs);
+	std::vector<glm::vec3>().swap(this->Normals);
+
+
+	std::vector<GLuint>().swap(this->Vertex_Indices);
+	std::vector<GLuint>().swap(this->UV_Indices);
+	std::vector<GLuint>().swap(this->Normal_Indices);
+
+
+	std::string head{};
+
+	while (!this->File.eof()) {
+		this->File >> head;
+		if (head[0] == '#') {
+			getline(this->File, head);
+		}
+		if (head[0] == 'v' && head[1] == 't') {
+			glm::vec2 uv{};
+			this->File >> uv.x >> uv.y;
+			this->UVs.push_back(uv);
+		}
+		else if (head[0] == 'v' && head[1] == 'n') {
+			glm::vec3 Normal{};
+
+			this->File >> Normal.x >> Normal.y >> Normal.z;
+			this->Normals.push_back(Normal);
+
+		}
+		else if (head[0] == 'v' && head.size() == 1) {
+			glm::vec3 Vertex_{};
+
+			this->File >> Vertex_.x >> Vertex_.y >> Vertex_.z;
+			this->Verties.push_back(Vertex_);
+		}
+		else if (head[0] == 'f') {
+
+			std::string Line[3]{};
+
+			GLuint VertexIndex[3]{};
+			GLuint UVIndec[3]{};
+			GLuint NormalIndex[3]{};
+
+			this->File >> Line[0] >> Line[1] >> Line[2];
+
+			for (auto i = 0; i < 3; ++i) {
+				std::istringstream iss(Line[i]);
+				std::string token{};
+				GLuint Data[3]{};
+				int dataindex = 0;
+				if (Line[i].find("//") != std::string::npos) {
+					// 있으면
+					std::getline(iss, token, '/');
+					this->Vertex_Indices.push_back(static_cast<GLuint>(stoi(token) - 1));
+
+
+					std::getline(iss, token, '/');
+					std::getline(iss, token, '/');
+
+
+					this->Normal_Indices.push_back(static_cast<GLuint>(stoi(token) - 1));
+				}
+				else {
+					//없으면 
+					std::getline(iss, token, '/');
+					this->Vertex_Indices.push_back(static_cast<GLuint>(stoi(token) - 1));
+
+
+					std::getline(iss, token, '/');
+					this->UV_Indices.push_back(static_cast<GLuint>(stoi(token) - 1));
+
+
+					std::getline(iss, token, '/');
+					this->Normal_Indices.push_back(static_cast<GLuint>(stoi(token) - 1));
+
+
+
+				}
+
+
+
+
+			}
+		}
+
+
+	}
+
+
+	size_t Vertexnum = this->Verties.size();
+
+	Color3f* BaseColor = new Color3f[Vertexnum];
+
+	for (auto i = 0; i < Vertexnum; ++i) {
+
+		if (this->RandomColor) {
+			BaseColor[i].r = this->ColorGenerater.RandF();
+			BaseColor[i].g = this->ColorGenerater.RandF();
+			BaseColor[i].b = this->ColorGenerater.RandF();
+		}
+		else {
+			BaseColor[i].r = 1.f;
+			BaseColor[i].g = 1.f;
+			BaseColor[i].b = 1.f;
+
+
+		}
+
+
+	}
+
+
+
+
+	for (const auto& i : this->Vertex_Indices) {
+		this->IndexedColor.push_back(BaseColor[i]);
+	}
+
+	delete[] BaseColor;
+}
+
+
+
+
+RETURNVOID Object3D::Buffering() {
+
+	glGenVertexArrays(1, &(this->VAO));
+	glBindVertexArray(this->VAO);
+
+
+	glGenBuffers(1, &(this->VBO.Position));
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO.Position);
+	glBufferData(GL_ARRAY_BUFFER, this->Verties.size() * sizeof(glm::vec3), &(this->Verties[0]), GL_STATIC_DRAW);
+
+	glGenBuffers(1, &(this->EBO));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->Vertex_Indices.size() * sizeof(unsigned int), &(this->Vertex_Indices[0]), GL_STATIC_DRAW);
+
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+
+	glGenBuffers(1, &(this->VBO.Color));
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO.Color);
+
+	glBufferData(GL_ARRAY_BUFFER, this->IndexedColor.size() * sizeof(Color3f), &(this->IndexedColor[0]), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glEnableVertexAttribArray(1);
+
+
+}
+
+
+
+RETURNVOID Object3D::Render() {
+
+	this->Culling ? glEnable(GL_DEPTH_TEST) : glDisable(GL_DEPTH_TEST);
+	this->Culling ? glEnable(GL_CULL_FACE) : glDisable(GL_CULL_FACE);
+	this->Filled ? glPolygonMode(GL_FRONT_AND_BACK, GL_FILL) : glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+	glm::mat4 Trans = glm::mat4(1.f);
+
+	Trans = glm::translate(Trans, this->Position);
+	Trans = glm::rotate(Trans, glm::radians(30.f + this->XRotate), glm::vec3(1.f, 0.f, 0.f));
+	Trans = glm::rotate(Trans, glm::radians(-30.f + this->YRotote), glm::vec3(0.f, 1.f, 0.f));
+	Trans = glm::scale(Trans, this->Scale);
+
+
+
+	GLuint WorldLocation = glGetUniformLocation(this->ShaderProgramId, "transform");
+	glUniformMatrix4fv(WorldLocation, 1, GL_FALSE, glm::value_ptr(Trans));
+
+	glBindVertexArray(this->VAO);
+
+	glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(this->Vertex_Indices.size()), GL_UNSIGNED_INT, 0);
+
+
+
+}
+
+
+RETURNVOID Object3D::SetProperty(Object3DBoolEnum Be, GLboolean Value) {
+	if (Be == Fill) {
+		this->Filled = Value;
+	}
+
+	if (Be == Cull) {
+		this->Culling = Value;
+	}
+}
+
+RETURNVOID Object3D::Transition(Object3DVecEnum Ve, glm::vec3 vec)
+{
+	if (Ve == RotateX) {
+		this->XRotate += vec.x;
+	}
+
+	if (Ve == RotateY) {
+
+		this->YRotote += vec.y;
+	}
+
+
+	if (Ve == Movement) {
+		this->Position = vec;
+	}
+
+	if (Ve == ObjectScale) {
+		this->Scale = vec;
+	}
+
+
+	return RETURNVOID();
+}
